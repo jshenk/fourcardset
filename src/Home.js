2c78ad83e584b56bebdfed60ab9f490549e9fe1b
Jenna Shenk <jshenk@DTC-60686.local>
2023-10-16 10:51:35 -0400

    Align shows

--- src/Home.js
+++ src/Home.js
@@ -26,47 +26,51 @@ const Home = () => {
         <div className="absolute left-2/3 top-1/4  z-10 w-[300px]">
           <img src={BandLogo} height="400px" width="400px" className="mb-16" />
           <a
-            className="flex items-center justify-between mb-6 text-xl text-white rounded-lg bg-white/10 hover:bg-stone-500"
+            className="flex items-center justify-between mb-6 text-xl text-white rounded-lg bg-stone-500 hover:bg-white/10"
             href="#contact"
           >
-            <span className="p-6">Book Us</span>
+            <span className="p-6 text-base">Contact Us</span>
             <span className="p-6 border-l">&#8594;</span>
           </a>
           <a
             className="flex items-center justify-between text-xl text-white rounded-lg bg-white/10 hover:bg-stone-500"
             href="#events"
           >
-            <span className="p-6">See Us Live</span>
+            <span className="p-6 text-base">See Us Live</span>
             <span className="p-6 border-l">&#8594;</span>
           </a>
         </div>
       </div>
 
-      <div className="flex max-w-5xl py-20 mx-auto">
-        <div className="w-2/3">
+      <div className="flex items-center justify-between max-w-5xl py-20 mx-auto">
+        <div className="w-1/2">
           <h2
             className="mb-10 text-3xl font-bold tracking-tight text-white"
             id="events"
           >
             See Us Live
           </h2>
-          <div className="max-w-xl">
+          <div className="">
             <Events />
           </div>
         </div>
-        <div className="w-1/3">
-          <img
-            src={Horse}
-            height="400px"
-            width="400px"
-            className="object-cover mb-8"
-          />
-          <img
-            src={Waterfront}
-            height="400px"
-            width="400px"
-            className="object-cover"
-          />
+        <div className="">
+          <div className="w-[400px] h-[400px] overflow-hidden mb-10">
+            <img
+              src={Horse}
+              height="400px"
+              width="400px"
+              className="w-auto h-full"
+            />
+          </div>
+          <div className="w-[400px] h-[400px] overflow-hidden">
+            <img
+              src={Waterfront}
+              height="400px"
+              width="400px"
+              className="w-auto h-full"
+            />
+          </div>
         </div>
       </div>
       <div className="max-w-5xl py-20 mx-auto border-t">
