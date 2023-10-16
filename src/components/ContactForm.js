cafb2ee53e657accde7955c2fbea1cd017a84933
Jenna Shenk <jshenk@DTC-60686.local>
2023-10-16 14:07:56 -0400

    Add form another way

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export const ContactForm = () => {
   return (
-    <form name="contact" netlify>
+    <form name="contact">
+      <input type="hidden" name="form-name" value="contact" />
+
       <div className="grid grid-cols-1 pb-12 border-b gap-x-8 gap-y-10 border-gray-900/10 md:grid-cols-3">
         <div>
           <h2 className="text-base font-semibold leading-7 text-white">
@@ -35,7 +37,6 @@ export const ContactForm = () => {
             </label>
             <div className="mt-2">
               <input
-                onChange={handleChange}
                 type="text"
                 name="first-name"
                 id="first-name"
@@ -54,7 +55,6 @@ export const ContactForm = () => {
             </label>
             <div className="mt-2">
               <input
-                onChange={handleChange}
                 type="text"
                 name="last-name"
                 id="last-name"
@@ -73,7 +73,6 @@ export const ContactForm = () => {
             </label>
             <div className="mt-2">
               <input
-                onChange={handleChange}
                 id="email"
                 name="email"
                 type="email"
@@ -84,16 +83,15 @@ export const ContactForm = () => {
           </div>
           <div className="col-span-full">
             <label
-              htmlFor="about"
+              htmlFor="message"
               className="block text-sm font-medium leading-6 text-white"
             >
               Message
             </label>
             <div className="mt-2">
               <textarea
-                onChange={handleChange}
-                id="about"
-                name="about"
+                id="message"
+                name="message"
                 rows={3}
                 className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 defaultValue={''}
@@ -105,7 +103,7 @@ export const ContactForm = () => {
               type="submit"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Button text
+              Submit
             </button>
           </div>
         </div>
