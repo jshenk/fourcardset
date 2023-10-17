67c49b8c9cb95c2df7d3c9a08a671ef5e21baa8d
Jenna Shenk <jshenk@DTC-60686.local>
2023-10-17 11:49:29 -0400

    Add mobile styling

--- src/Events.js
+++ src/Events.js
@@ -3,26 +3,25 @@ const cards = [
     name: 'Waterfront Hotel',
     location: 'Balitmore, MD',
     description: 'October 21 @ 9:00 pm - 1:00 am',
+    id: 1,
   },
   {
     name: 'Waterfront Hotel',
     location: 'Balitmore, MD',
     description: 'November 11 @ 9:00 pm - 1:00 am',
+    id: 2,
   },
   {
     name: 'The Horse You Came In On Saloon',
     location: 'Balitmore, MD',
     description: 'November 18 @ 10:00 pm - 1:00 am',
-  },
-  {
-    name: 'Christmas Show',
-    location: 'Dulles, VA',
-    description: 'December 2 @ 9:00 am',
+    id: 3,
   },
   {
     name: 'The Horse You Came In On Saloon',
     location: 'Balitmore, MD',
     description: 'December 9 @ 10:00 pm - 1:00 am',
+    id: 4,
   },
 ];
 
@@ -30,9 +29,9 @@ const Events = () => {
   return (
     <div className="divide-y rounded bg-stone-500">
       {cards.map((card) => (
-        <div key={card.name} className="flex p-6">
+        <div key={card.id} className="flex p-6">
           <div className="w-full text-base leading-7">
-            <div className="flex items-center justify-between">
+            <div className="items-center justify-between lg:flex">
               <h3 className="font-bold text-white">{card.name}</h3>
               <div className="text-xs text-white uppercase">
                 {card.location}
--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ import Events from './Events';
 const Home = () => {
   return (
     <div className="bg-gray-900">
-      <div>
+      <div className="relative">
         <video
           width="320"
           height="240"
@@ -18,50 +18,50 @@ const Home = () => {
           autoPlay
           muted
           loop
-          className="object-cover w-3/5 h-screen overflow-hidden"
+          className="object-cover w-full overflow-hidden h-[400px] lg:h-screen lg:w-3/5"
         >
           <source src={BannerVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="absolute left-2/3 top-1/4  z-10 w-[300px]">
+        <div className="lg:absolute lg:left-2/3 lg:top-1/4 z-10 lg:w-[300px] px-5 py-10 lg:p-0">
           <img
             src={BandLogo}
             height="400px"
             width="400px"
-            className="mb-16"
+            className="absolute max-w-[200px] mx-auto lg:max-w-[300px] mb-16 lg:w-auto left-0 right-0 top-24 lg:static lg:top-auto lg:left-auto"
             alt="homepage banner"
           />
           <a
             className="flex items-center justify-between mb-6 text-xl text-white rounded-lg bg-stone-500 hover:bg-white/10"
             href="#contact"
           >
-            <span className="p-6 text-base">Contact Us</span>
-            <span className="p-6 border-l">&#8594;</span>
+            <span className="p-4 text-base lg:p-6">Contact Us</span>
+            <span className="p-4 border-l lg:p-6">&#8594;</span>
           </a>
           <a
             className="flex items-center justify-between text-xl text-white rounded-lg bg-white/10 hover:bg-stone-500"
             href="#events"
           >
-            <span className="p-6 text-base">See Us Live</span>
-            <span className="p-6 border-l">&#8594;</span>
+            <span className="p-4 text-base lg:p-6">See Us Live</span>
+            <span className="p-4 border-l lg:p-6">&#8594;</span>
           </a>
         </div>
       </div>
 
-      <div className="flex items-center justify-between max-w-5xl py-20 mx-auto">
-        <div className="w-1/2">
+      <div className="flex flex-col items-center justify-between max-w-5xl px-5 py-10 mx-auto lg:flex-row lg:py-20 lg:px-0">
+        <div className="w-full mb-10 lg:w-1/2 lg:mb-0">
           <h2
             className="mb-10 text-3xl font-bold tracking-tight text-white"
             id="events"
           >
             See Us Live
           </h2>
-          <div className="">
+          <div>
             <Events />
           </div>
         </div>
-        <div className="">
-          <div className="w-[400px] h-[400px] overflow-hidden mb-10">
+        <div>
+          <div className="lg:w-[400px] lg:h-[400px] lg:overflow-hidden mb-10">
             <img
               src={Horse}
               height="400px"
@@ -70,7 +70,7 @@ const Home = () => {
               alt="horse you came in on"
             />
           </div>
-          <div className="w-[400px] h-[400px] overflow-hidden">
+          <div className="lg:w-[400px] lg:h-[400px] lg:overflow-hidden">
             <img
               src={Waterfront}
               height="400px"
@@ -81,9 +81,9 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="max-w-5xl py-20 mx-auto border-t">
+      <div className="max-w-5xl px-5 pt-10 mx-auto border-t lg:py-20 lg:px-0">
         <h2
-          className="text-3xl font-bold tracking-tight text-white"
+          className="mb-5 text-3xl font-bold tracking-tight text-white lg:mb-0"
           id="contact"
         >
           Contact Us
