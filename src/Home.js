35aabc6fde5f0fad6699d36f95e7042ca2400d19
Jenna Shenk <jshenk@DTC-60686.local>
2023-10-17 12:28:15 -0400

    Fix video for safari

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import BannerVideo from './assets/BannerVideo.webm';
+import BannerVideoMP4 from './assets/BannerVideoMP4.mp4';
 import BandLogo from './assets/BandLogo.png';
 import Horse from './assets/Horse.jpg';
 import Waterfront from './assets/Waterfront.jpeg';
@@ -10,7 +11,7 @@ import Events from './Events';
 const Home = () => {
   return (
     <div className="bg-gray-900">
-      <div className="relative">
+      <div className="relative lg:flex">
         <video
           width="320"
           height="240"
@@ -18,33 +19,36 @@ const Home = () => {
           autoPlay
           muted
           loop
-          className="object-cover w-full overflow-hidden h-[400px] lg:h-screen lg:w-3/5"
+          className="object-cover w-full overflow-hidden h-[400px] lg:h-screen lg:w-2/3"
         >
-          <source src={BannerVideo} type="video/mp4" />
+          <source src={BannerVideo} type="video/webm" />
+          <source src={BannerVideoMP4} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="lg:absolute lg:left-2/3 lg:top-1/4 z-10 lg:w-[300px] px-5 py-10 lg:p-0">
+        <div className="z-10 px-5 py-10 align-middle lg:h-screen lg:w-1/3 lg:flex lg:flex-wrap lg:items-center lg:pb-3">
           <img
             src={BandLogo}
             height="400px"
             width="400px"
-            className="absolute max-w-[200px] mx-auto lg:max-w-[300px] mb-16 lg:w-auto left-0 right-0 top-24 lg:static lg:top-auto lg:left-auto"
+            className="absolute max-w-[200px] mx-auto lg:max-w-[300px]  lg:w-auto left-0 right-0 top-24 lg:static lg:top-auto lg:left-auto"
             alt="homepage banner"
           />
-          <a
-            className="flex items-center justify-between mb-6 text-xl text-white rounded-lg bg-stone-500 hover:bg-white/10"
-            href="#contact"
-          >
-            <span className="p-4 text-base lg:p-6">Contact Us</span>
-            <span className="p-4 border-l lg:p-6">&#8594;</span>
-          </a>
-          <a
-            className="flex items-center justify-between text-xl text-white rounded-lg bg-white/10 hover:bg-stone-500"
-            href="#events"
-          >
-            <span className="p-4 text-base lg:p-6">See Us Live</span>
-            <span className="p-4 border-l lg:p-6">&#8594;</span>
-          </a>
+          <div className="w-full">
+            <a
+              className="flex items-center justify-between mb-6 text-xl text-white rounded-lg bg-stone-500 hover:bg-white/10 max-w-[300px] mx-auto"
+              href="#contact"
+            >
+              <span className="p-4 text-base lg:p-6">Contact Us</span>
+              <span className="p-4 border-l lg:p-6">&#8594;</span>
+            </a>
+            <a
+              className="flex items-center justify-between text-xl text-white rounded-lg bg-white/10 hover:bg-stone-500 max-w-[300px] mx-auto"
+              href="#events"
+            >
+              <span className="p-4 text-base lg:p-6">See Us Live</span>
+              <span className="p-4 border-l lg:p-6">&#8594;</span>
+            </a>
+          </div>
         </div>
       </div>
 
@@ -66,7 +70,6 @@ const Home = () => {
               src={Horse}
               height="400px"
               width="400px"
-              className="w-auto h-full"
               alt="horse you came in on"
             />
           </div>
@@ -75,7 +78,6 @@ const Home = () => {
               src={Waterfront}
               height="400px"
               width="400px"
-              className="w-auto h-full"
               alt="waterfront hotel"
             />
           </div>
@@ -83,7 +85,7 @@ const Home = () => {
       </div>
       <div className="max-w-5xl px-5 pt-10 mx-auto border-t lg:py-20 lg:px-0">
         <h2
-          className="mb-5 text-3xl font-bold tracking-tight text-white lg:mb-0"
+          className="mb-5 text-3xl font-bold tracking-tight text-white"
           id="contact"
         >
           Contact Us
