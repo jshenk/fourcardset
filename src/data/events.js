210fe2f2d9aee25c791f9929d174d2a9559c304d
Jenna Shenk <jshenk@DTC-60686.local>
2024-07-29 10:28:36 -0400

    Update events

--- src/data/events.js
+++ src/data/events.js
@@ -7,16 +7,10 @@ export const cards = [
     id: 1,
   },
   {
-    name: 'Private Event',
-    location: 'Balitmore, MD',
-    date: '2024-08-31T21:30:00',
-    id: 2,
-  },
-  {
     name: 'Waterfront Hotel',
     location: 'Balitmore, MD',
     date: '2024-10-25T21:30:00',
     link: 'https://maps.app.goo.gl/4P2Bvgi5fREt5PSE8',
-    id: 3,
+    id: 2,
   },
 ];
