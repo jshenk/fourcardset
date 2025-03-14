2069c8e8d6e0d9e049a7eccf62cfcf39be2c5574
fourcardset <4cardset@gmail.com>
2025-03-14 06:34:41 -0400

    Update events.js

--- src/data/events.js
+++ src/data/events.js
@@ -1,23 +1,23 @@
 export const cards = [
   {
-    name: 'Baltimore Taco Festival Power Plant Live! - Main Stage',
+    name: 'The Worthington',
     location: 'Balitmore, MD',
-    date: '2024-10-05T14:30:00',
-    link: 'https://maps.app.goo.gl/1YDd6yD4wrV7bL8Z7',
+    date: '2025-03-28T19:00:00',
+    link: 'https://maps.app.goo.gl/yrGv1dk3YVZuEh6U6',
     id: 1,
-  },
+  },  
+{
+    name: 'Monument City Brewing',
+    location: 'Balitmore, MD',
+    date: '2025-06-14T18:00:00',
+    link: 'https://maps.app.goo.gl/HhZZYSpCfqZw3rsVA',
+    id: 2,
+},
   {
     name: 'Waterfront Hotel',
     location: 'Balitmore, MD',
-    date: '2024-10-25T21:30:00',
+    date: '2025-06-21T21:30:00',
     link: 'https://maps.app.goo.gl/4P2Bvgi5fREt5PSE8',
-    id: 2,
-  },
-  {
-    name: 'Monument City Brewing W/ Los Fellas',
-    location: 'Balitmore, MD',
-    date: '2024-11-16T19:00:00',
-    link: 'https://maps.app.goo.gl/HhZZYSpCfqZw3rsVA',
     id: 3,
   },
 ];
