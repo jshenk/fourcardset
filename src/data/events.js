ada868eca269b29192968ca05240243e0af79e95
fourcardset <4cardset@gmail.com>
2025-05-29 14:42:41 -0400

    Update events.js
    Add Waterfront and Horse dates

--- src/data/events.js
+++ src/data/events.js
@@ -1,23 +1,37 @@
 export const cards = [
   {
-    name: 'The Worthington',
-    location: 'Balitmore, MD',
-    date: '2025-03-28T21:00:00',
-    link: 'https://maps.app.goo.gl/yrGv1dk3YVZuEh6U6',
-    id: 1,
-  },  
-{
     name: 'Monument City Brewing',
     location: 'Balitmore, MD',
     date: '2025-06-14T18:00:00',
     link: 'https://maps.app.goo.gl/HhZZYSpCfqZw3rsVA',
-    id: 2,
-},
+    id: 1,
+  },
   {
     name: 'Waterfront Hotel',
     location: 'Balitmore, MD',
     date: '2025-06-21T21:30:00',
     link: 'https://maps.app.goo.gl/4P2Bvgi5fREt5PSE8',
+    id: 2,
+  },  
+  {
+    name: 'Waterfront Hotel',
+    location: 'Balitmore, MD',
+    date: '2025-08-23T21:30:00',
+    link: 'https://maps.app.goo.gl/4P2Bvgi5fREt5PSE8',
     id: 3,
-  },
+  },  
+  {
+    name: 'Waterfront Hotel',
+    location: 'Balitmore, MD',
+    date: '2025-09-05T21:30:00',
+    link: 'https://maps.app.goo.gl/4P2Bvgi5fREt5PSE8',
+    id: 4,
+  }, 
+  {
+    name: 'The Horse You Came In On',
+    location: 'Balitmore, MD',
+    date: '2025-09-13T21:30:00',
+    link: 'https://maps.app.goo.gl/NnZY17KGnMnDro937',
+    id: 5,
+  },  
 ];
