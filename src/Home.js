c5e2950e88c996f426f5c167c571a33a7f09e707
fourcardset <4cardset@gmail.com>
2026-05-23 15:14:27 -0400

    Update Home.js
    Runaway writeup

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import AlbumTree from './assets/AlbumTree.JPEG';
+import AlbumTree from './assets/RunawayCover.PNG';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -21,7 +21,7 @@ const Home = () => {
             </h2>
             <a
               className="px-6 py-3 font-bold text-white uppercase transition-colors duration-500 border-2 border-amber-500 hover:text-amber-500"
-              href="https://open.spotify.com/album/5kIGwHeDIbj05s3yCsabLe?si=gbCjHQSXTrWMpNVo7m_P7Q"
+              href="https://open.spotify.com/track/6ebUBlzWWZsGetQy0sSZWO?si=8b9d97e9440a4d51"
             >
               Stream Now
             </a>
@@ -42,17 +42,21 @@ const Home = () => {
           <p className="mb-6">
             Formed by four work friends, the Indie/Alternative rock band has
             quickly ascended in the Maryland music scene. Fourcardset is lead by
-            pianist/vocalist Ben Lucier, and accompanied by guitarist Joe
+            multi-instrumentalist/vocalist Ben Lucier, Joe
             Consoli, bassist Cody Tilson, and drummer Austin Freilich. With
             roots in various genres of music, the group brings a dynamic,
             multifaceted sound to their audience.
           </p>
           <p className="mb-12">
-            In their debut EP - Overtime, fourcardset focuses in on a diverse
-            but cohesive sound that is “true to themselves” and highlights each
-            individuals' musical background and interests. Written by Ben, the
-            self produced EP explores themes of past lives, childhood, and
-            navigating through time.
+            Serving as the high-energy follow-up to their debut EP, 
+            <a href="https://open.spotify.com/album/5kIGwHeDIbj05s3yCsabLe?si=nnlu1c9_TsS_jkAAVNhdLg">Overtime</a>
+            , fourcardset’s latest single "Runaway" dives headfirst into the 
+            beautiful chaos of falling in love. Built on a foundation of infectious 
+            musical motifs and undeniable hooks, the track delivers a pulse-driven 
+            experience that mirrors the exact, heart-pounding rush of letting your 
+            guard down for someone new. It's an anthem about vulnerability, 
+            unexpected change, and the thrill of letting love completely shift your world.
+            
           </p>
           <a
             className="px-4 py-2 mr-6 text-black uppercase transition-colors duration-500 bg-white md:mr-12 hover:bg-amber-500 hover:text-white"
